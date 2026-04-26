a053d878d5d1fddf48cd7511fe65fffa32ffc07e
Jean Bernard <167228633+adam-MF-git@users.noreply.github.com>
2026-04-26 22:09:11 +0200

    modification de macro.hh et Registre.hh

--- App/Registre.hh
+++ App/Registre.hh
@@ -1,10 +1,13 @@
 #pragma once
 #include <memory>
-#include "Joueur.hh"
+#include <Joueur/Joueur.hh>
+#include <Jeu/Jeu.hh>
 #include <vector>
 
 using PtrJoueur = std::unique_ptr<Joueur>;
-using FnCreationJoueur = PtrJoueur(*)();
+using FnCreationJoueur = PtrJoueur(*)(std::string,bool);
+using PtrJeu = std::unique_ptr<Jeu>;
+using FnCreationJeu = PtrJeu(*)();
 
 class Registre {
 private:
@@ -12,7 +15,8 @@ private:
     ~Registre()=default;
     Registre(Registre const &) = delete;
     Registre & operator=(Registre const &) = delete;
-    std::vector<FnCreationJoueur> _VCFJ;
+    std::vector<FnCreationJoueur> _VCFJo;
+    std::vector<FnCreationJeu> _VCFJe;
 public:
     static Registre& instance() {
         static Registre le_registre;
@@ -20,17 +24,30 @@ public:
     }
 
     static bool ajouter_dans_registre(FnCreationJoueur CFJ) {
-        instance()._VCFJ.push_back(CFJ);
+        instance()._VCFJo.push_back(CFJ);
+        return true;
+    }
+    static bool ajouter_dans_registre(FnCreationJeu CFJ) {
+        instance()._VCFJe.push_back(CFJ);
         return true;
     }
 
-    static size_t nb_classes_dans_registre() {
-        return instance()._VCFJ.size();
+    static size_t nb_classes_dans_registre_Joueur() {
+        return instance()._VCFJo.size();
+    }
+    static size_t nb_classes_dans_registre_Jeu() {
+        return instance()._VCFJe.size();
     }
 
-    static PtrJoueur getNouveauJoueur(size_t position) {
-        if (position<nb_classes_dans_registre())
-            return instance()._VCFJ[position]();
+    static PtrJoueur getNouveauJoueur(size_t position,std::string nom, bool joueur_1) {
+        if (position<nb_classes_dans_registre_Joueur())
+            return instance()._VCFJo[position](nom,joueur_1);
+        else
+            return nullptr;
+    }
+    static PtrJeu getNouveauJeu(size_t position) {
+        if (position<nb_classes_dans_registre_Joueur())
+            return instance()._VCFJe[position]();
         else
             return nullptr;
     }
--- App/macro.hh
+++ App/macro.hh
@@ -3,8 +3,16 @@
 
 #define ENREGISTRER_JOUEUR(CLASS)                                                               \
 namespace {                                                                                     \
-    static PtrJoueur create_##CLASS() {                                                         \
-        return std::make_unique<CLASS>();                                                       \
+    static PtrJoueur create_##CLASS(std::string nom,bool joueur_1) {                            \
+        return std::make_unique<CLASS>(nom,joueur_1);                                           \
     }                                                                                           \
+    static bool enregister_##CLASS = Registre::ajouter_dans_registre( create_##CLASS );         \
+}
+
+#define ENREGISTRER_JEU(CLASS)                                                                  \
+namespace {                                                                                     \
+    static PtrJeu create_##CLASS() {                                                            \
+        return std::make_unique<CLASS>();                                                       \
+}                                                                                               \
     static bool enregister_##CLASS = Registre::ajouter_dans_registre( create_##CLASS );         \
 }
